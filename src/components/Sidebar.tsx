d93001de8289d91fbefb39b7a27a2db22463e82c
Anson Chan <chananson1712@gmail.com>
2024-10-02 18:22:18 -0700

    FEAT: Sidebar (Do i really need this though? not really...)

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,27 @@
+import { BookmarkIcon } from "@radix-ui/react-icons";
+import { HouseIcon, SettingsIcon, TelescopeIcon } from "lucide-react";
+import React, { useState } from "react";
+import { iconSize } from "@/constants/iconSize";
+
+const listContainer = "flex flex-row gap-2 items-center justify-center border-b px-8 py-4";
+
+export default function Sidebar() {
+  return (
+    <>
+      <ul className="flex flex-col w-64 h-screen mt-20 border-r text-xl fixed top-0 left-0 transition-all duration-300 ease-in-out overflow-hidden">
+        <li className={`${listContainer} border-t`}>
+          <HouseIcon className={iconSize} />
+          <span>Home</span>
+        </li>
+        <li className={listContainer}>
+          <TelescopeIcon className={iconSize} />
+          <span>Explore</span>
+        </li>
+        <li className={listContainer}>
+          <SettingsIcon className={iconSize} />
+          <span>Settings</span>
+        </li>
+      </ul>
+    </>
+  );
+}
